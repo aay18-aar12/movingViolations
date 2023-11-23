07fa6c99763c888c33319f8d5161aeec39a05d12
aay18-aar12 <133048419+aay18-aar12@users.noreply.github.com>
2023-11-23 14:22:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def retTime(issue_time, page):
 
     
     ko = (page*1000)-1000
-    request = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Violations_Moving_2023/MapServer/4/query?where=1%3D1&ISSUE_TIME%3D' + issue_time + '&outFields=*&outSR=4326&resultOffset='+str(ko) +'&f=json')
+    request = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Violations_Moving_2023/MapServer/4/query?where=ISSUE_TIME%3D%27' + issue_time + '%27&outFields=*&outSR=4326&resultOffset='+str(ko) +'&f=json')
     bo = request.json()
     return bo
     
@@ -29,7 +29,7 @@ def retTime(issue_time, page):
 def retFine(fine_amount, page):
 
     ko = (page*1000)-1000
-    request = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Violations_Moving_2023/MapServer/4/query?where=1%3D1&FINE_AMOUNT%3D' + fine_amount + '&outFields=*&outSR=4326&resultOffset='+str(ko) +'&f=json')
+    request = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Violations_Moving_2023/MapServer/4/query?where=FINE_AMOUNT%3D%27' + fine_amount + '%27&outFields=*&outSR=4326&resultOffset='+str(ko) +'&f=json')
     bo = request.json()
     return bo
     
@@ -37,10 +37,9 @@ def retFine(fine_amount, page):
 @app.route('/total/<string:total>/<int:page>')
 def retTotal(total, page):
     ko = (page*1000)-1000
-    request = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Violations_Moving_2023/MapServer/4/query?where=1%3D1&TOTAL_PAID%3D' + total + '&outFields=*&outSR=4326&resultOffset='+str(ko) +'&f=json')
+    request = requests.get('https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Violations_Moving_2023/MapServer/4/query?where=TOTAL_PAID%3D%27' + total + '%27&outFields=*&outSR=4326&resultOffset='+str(ko) +'&f=json')
     bo = request.json()
     return bo
-
 
 @app.route('/health')
 def retHealth():
